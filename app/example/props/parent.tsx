3369b4d9240903f177458f5ec560fc3cf0c9281f
diwasbk <diwasbk903@gmail.com>
2026-01-21 21:48:18 +0545

    Add ParentComponent with props for ChildComponent

---
+++ app/example/props/parent.tsx
@@ -0,0 +1,11 @@
+import ChildComponent from "./child";
+
+export default function ParentComponent() {
+    return (
+        <ChildComponent
+            name="Diwas BK"
+            role="Full Stack Developer"
+            interest="I love building modern web apps with clean UI and powerful backend systems."
+        />
+    );
+}
