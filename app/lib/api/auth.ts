b1d01257ba2189b9290e640ab8776337106ce706
diwasbk <diwasbk903@gmail.com>
2026-02-07 21:45:37 +0545

    Implement send password reset email and reset password API calls

--- app/lib/api/auth.ts
+++ app/lib/api/auth.ts
@@ -1,6 +1,7 @@
 import { loginType } from "@/app/(auth)/login/schema";
 import axiosInstance from "./axios";
 import { API } from "./endpoints";
+import { resetPasswordType, sendPasswordResetEmailType } from "@/app/(auth)/schems";
 
 export const loginUser = async (data: loginType) => {
     try {
@@ -9,5 +10,27 @@ export const loginUser = async (data: loginType) => {
 
     } catch (err: Error | any) {
         throw new Error(err.response?.data?.message || err.message || "Login failed!");
+    }
+}
+
+export const sendPasswordResetEmail = async (data: sendPasswordResetEmailType) => {
+    try {
+        const response = await axiosInstance.post(API.auth.forgot_password, data);
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message || err.message || "Failed to send password reset email!"
+        )
+    }
+}
+
+export const resetAccountPassword = async (data: resetPasswordType) => {
+    try {
+        const response = await axiosInstance.put(API.auth.reset_password, data);
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message || err.message || "Failed to reset password!"
+        )
     }
 }
