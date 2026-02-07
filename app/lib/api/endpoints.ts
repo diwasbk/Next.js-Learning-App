1a48228f27962a527b1abb9a32cc951e0f8858e7
diwasbk <diwasbk903@gmail.com>
2026-02-07 21:43:59 +0545

    Add forgot and reset password API endpoints

--- app/lib/api/endpoints.ts
+++ app/lib/api/endpoints.ts
@@ -1,6 +1,8 @@
 export const API = {
     auth: {
-        login: "/api/auth/login"
+        login: "/api/auth/login",
+        forgot_password: "/api/auth/send-password-reset-email",
+        reset_password: "/api/auth/reset-account-password"
     },
     kyc: {
         fillKyc: "/api/kyc/fill",
